fd8b32bfe4315bd563951728a00bc5c2a855ef42
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2023-09-30 13:15:38 -0300

    Ordenando: testando os metodos

---
+++ src/main/java/list/Ordenacao/Pessoa.java
@@ -0,0 +1,49 @@
+package main.java.list.Ordenacao;
+
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa>{
+    private String name;
+
+    private int idade;
+
+    private double altura;
+
+    public Pessoa(String name, int idade, double altura) {
+        this.name = name;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    @Override
+    public int compareTo(Pessoa o) {
+        return Integer.compare(idade,o.getIdade());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "name='" + name + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                '}';
+    }
+    static class ComparatorPorAltura implements Comparator<Pessoa> {
+        @Override
+        public int compare(Pessoa p1, Pessoa p2) {
+            return Double.compare(p1.getAltura(), p2.getAltura());
+        }
+    }
+}
